829c1c68faaec79d0d7616ca972e338ff6d60458
Rita Snow <hikaru.snow@yandex.ru>
2023-01-19 18:53:17 +0300

    Initial commit

---
+++ Task1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = scanner.nextInt();
+        if (number > 7) {
+            System.out.println("Привет");
+        } else {
+            System.out.println("Число должно быть больше 7");
+        }
+        scanner.close();
+    }
+}
---
+++ Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for(int i = 1; i<array.length; i++){
+            array[i] = i*3;
+            System.out.println(array[i]);
+        }
+    }
+}
